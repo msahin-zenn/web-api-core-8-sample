d2040372af4bbff59519c7c782b5a5b55ee285a0
Mehmet Ali Şahin <msahin@zenn.com.tr>
2024-03-25 00:02:29 +0300

    feature: activate swagger for production host

--- WebApiCore8Sample/Program.cs
+++ WebApiCore8Sample/Program.cs
@@ -17,11 +17,8 @@ namespace WebApiCore8Sample
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
